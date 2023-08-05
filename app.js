c5c792d1f8e89d71b6dc42232b09ac796295273c
skfeat <128950274+skfeat@users.noreply.github.com>
2023-08-05 16:47:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const cors = require('cors'); // Import the cors middleware
 
-app.get("/", (req, res) => res.type('html').send(html));
+const signup = require('./routes/Signup');
+const login = require('./routes/Login');
+const allstudents = require('./routes/allstudents');
+const profile = require('./routes/profile');
+const updateresult = require('./routes/UpdateResult');
+const getresult = require('./routes/getresult');
+const previousresult = require('./routes/PreviousResult');
+const notes = require('./routes/Notes');
+const updatenotes = require('./routes/UpdateNotes');
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(cors()); // Enable CORS for all routes
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.use("/signup", signup);
+app.use("/login", login);
+app.use("/allstudents", allstudents);
+app.use("/profile", profile);
+app.use("/updateresult", updateresult);
+app.use("/getresult", getresult);
+app.use("/previousresult", previousresult);
+app.use("/notes", notes);
+app.use("/updatenotes", updatenotes);
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+app.listen(3006, () => {
+  console.log("Server is running on port 3000");
+});
